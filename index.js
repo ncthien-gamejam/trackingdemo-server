f0977c3c934f3a5b077a9263bbf21bf44f61e864
Nguyen Chi Thien <ncthien@gamejam.co>
2023-07-17 14:09:44 +0700

    Add back root path

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const PORT = process.env.PORT || 5001
 
 var app = express();
 
+app.get('/', function (req, res) {
+   res.send('Hello World');
+})
+
 app.use(express.static(path.join(__dirname, 'public')))
     
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
