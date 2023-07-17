e50fda604d7ace1c6abd786e39b809ec83bcf460
Nguyen Chi Thien <ncthien@gamejam.co>
2023-07-17 12:21:48 +0700

    Add AASA

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ const path = require('path')
 
 const PORT = process.env.PORT || 5001
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+var app = express();
+
+app.get('/', function (req, res) {
+   res.send('Hello World');
+})
+
+app.use(express.static(path.join(__dirname, 'public')))
+    
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
