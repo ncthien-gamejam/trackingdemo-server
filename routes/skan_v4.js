8c2149722af6e75aaf52a488a6a463873c0e816f
Nguyễn Chí Thiện <ncthien@superfine.org>
2024-03-26 15:45:30 +0700

    Finish implementing conversion tool for SKAN v4

--- routes/skan_v4.js
+++ routes/skan_v4.js
@@ -19,7 +19,348 @@ function getEventMapping(eventName)
 
 const baseCurrency = 'USD';
 
-function createEventData(baseObject)
+const appStoreId = 6451180535;
+const updatedTime = 1711437911;
+
+const EPS = 1e-7;
+
+var groupBy = function(xs, key) {
+  return xs.reduce(function(rv, x) {
+    (rv[x[key]] = rv[x[key]] || []).push(x);
+    return rv;
+  }, {});
+};
+
+function swapElements(arr, id0, id1)
+{
+    const tmp = arr[id0];
+    arr[id0] = arr[id1];
+    arr[id1] = tmp;
+}
+
+function compareEventRect(rectA, rectB)
+{
+	//Check min revenue
+	if (rectA[0] < rectB[0] - EPS) return -1;
+	if (rectA[0] > rectB[0] + EPS) return 1;
+	
+	//Check min count
+	if (rectA[2] < rectB[2]) return -1;
+	if (rectA[2] > rectB[2]) return 1;
+	
+	return 0;
+}
+
+function containsRevenueRange(rectA, rectB)
+{
+	if (rectA[0] > rectB[0] + EPS) return false;
+	
+	if (rectA[1] > -EPS) //not infinity
+	{
+		if (rectB[1] < -EPS) return false; //infinity
+		if (rectA[1] < rectB[1] - EPS) return false;
+	}
+	
+	return true;
+}
+
+function containsCountRange(rectA, rectB)
+{
+	if (rectA[2] > rectB[2]) return false;
+	
+	if (rectA[3] >= 0) //not infinity
+	{
+		if (rectB[3] < 0) return false;  //infinity
+		if (rectA[3] < rectB[3]) return false;
+	}
+	
+	return true;
+}
+
+function containsRect(rectA, rectB)
+{
+	if (rectA[0] > rectB[0] + EPS) return false;
+	
+	if (rectA[1] > -EPS) //not infinity
+	{
+		if (rectB[1] < -EPS) return false;  //infinity
+		if (rectA[1] < rectB[1] - EPS) return false;
+	}
+	
+	if (rectA[2] > rectB[2]) return false;
+	
+	if (rectA[3] >= 0) //not infinity
+	{
+		if (rectB[3] < 0) return false;  //infinity
+		if (rectA[3] < rectB[3]) return false;
+	}
+	
+	return true;
+}
+
+function disjointRect(rectA, rectB)
+{
+	if (rectB[1] > -EPS) //not infinity
+	{
+		if (rectA[0] > rectB[1] + EPS) return true;
+	}
+	
+	if (rectA[1] > -EPS) //not infinity
+	{
+		if (rectB[0] > rectA[1] + EPS) return true;
+	}
+	
+	if (rectB[3] >= 0) //not infinity
+	{
+		if (rectA[2] > rectB[3] + 1) return true;
+	}
+	
+	if (rectA[3] >= 0) //not infinity
+	{
+		if (rectB[2] > rectA[3] + 1) return true;
+	}
+	
+	return false;
+}
+
+function adjustTopEventRect(rects, pos)
+{
+	let move = false;
+	
+	let num = rects.length;
+	while (pos + 1 < num && compareEventRect(rects[pos], rects[pos + 1]) > 0)
+	{
+		swapElements(rects, pos, pos + 1);
+		pos++;
+		
+		move = true;
+	}
+	
+	return move;
+}
+
+//TODO: Test different cases
+function groupEventsData(eventName, eventsData)
+{
+	let rects = [];
+	
+	let numEvents = eventsData.length;
+	for (let i = 0; i < numEvents; ++i)
+	{
+		let eventData = eventsData[i];
+		
+		let revenueMin = eventData['revenue_min'] || 0.0;
+		let revenueMax = eventData['revenue_max'] || -1.0;
+		
+		let countMin = eventData['count_min'] || 1;
+		let countMax = eventData['count_max'] || -1;
+
+		rects.push([revenueMin, revenueMax, countMin, countMax]);
+	}
+	
+	//Sort rects based on min revenue and min count
+	for (let i = 0; i < numEvents; ++i)
+	{
+		for (let j = i + 1; j < numEvents; ++j)
+		{
+			if (compareEventRect(rects[i], rects[j]) > 0)
+			{
+				swapElements(rects, i, j);
+			}
+		}
+	}
+	
+	let ret = [];
+	
+	for (let i = 0; i < numEvents; ++i)
+	{
+		let curRect = rects[i];
+		
+		let valid = true;
+		
+		for (let j = i + 1; j < numEvents; ++j)
+		{
+			let targetRect = rects[j];
+			
+			if (containsRect(curRect, targetRect))
+			{
+				rects.splice(j, 1);
+				
+				j--;
+				numEvents--;
+				
+				continue;
+			}
+	
+			if (containsRect(targetRect, curRect)) //Remove rect that is fully inside other rect
+			{
+				valid = false;
+				break;
+			}
+	
+			if (disjointRect(targetRect, curRect))
+			{
+				continue;
+			}
+			
+			if (containsRevenueRange(targetRect, curRect))
+			{
+				//Increase current rect's size
+				if (curRect[3] >= 0) //not infinity
+				{
+					if (targetRect[3] < 0) //infinity
+					{
+						curRect[3] = -1;
+					}
+					else
+					{
+						curRect[3] = Math.max(curRect[3], targetRect[3]);
+					}
+				}
+				
+				if (curRect[1] < -EPS) //infinity
+				{
+					//Remove target rect
+					rects.splice(j, 1);
+				
+					j--;
+					numEvents--;
+				}
+				else
+				{
+					targetRect[0] = Math.max(targetRect[0], curRect[1]);
+					if (targetRect[1] > -EPS && targetRect[0] > targetRect[1] - EPS)
+					{
+						//Remove target rect
+						rects.splice(j, 1);
+					
+						j--;
+						numEvents--;
+					}
+					else
+					{
+						rects[j] = targetRect;
+						if (adjustTopEventRect(rects, j))
+						{
+							j--;
+						}
+					}
+				}
+			}
+			else if (containsCountRange(targetRect, curRect))
+			{
+				//Increase current rect's size
+				if (curRect[1] > -EPS) //not infinity
+				{
+					if (targetRect[1] < -EPS) //infinity
+					{
+						curRect[1] = -1.0;
+					}
+					else
+					{
+						curRect[1] = Math.max(curRect[1], targetRect[1]);
+					}
+				}
+				
+				
+				if (curRect[3] < 0) //infinity
+				{
+					//Remove target rect
+					rects.splice(j, 1);
+				
+					j--;
+					numEvents--;
+				}
+				else
+				{
+					targetRect[2] = Math.max(targetRect[2], curRect[3] + 1);
+					if (targetRect[3] >= 0 && targetRect[2] > targetRect[3])
+					{
+						//Remove target rect
+						rects.splice(j, 1);
+					
+						j--;
+						numEvents--;
+					}
+					else
+					{
+						rects[j] = targetRect;
+						if (adjustTopEventRect(rects, j))
+						{
+							j--;
+						}
+					}
+				}
+			}
+		}
+		
+		if (valid)
+		{
+			let eventData = {'event_name': eventName};
+			
+			if (curRect[0] > EPS)
+			{
+				eventData['revenue_min'] = curRect[0];
+			}
+			
+			if (curRect[1] > -EPS)
+			{
+				eventData['revenue_max'] = curRect[1];
+			}
+			
+			if (curRect[2] > 1)
+			{
+				eventData['count_min'] = curRect[2];
+			}
+			
+			if (curRect[3] >= 0)
+			{
+				eventData['count_max'] = curRect[3];
+			}
+			
+			ret.push(eventData);
+		}
+	}
+	
+	if (ret.length === 0) return null;
+	
+	return ret;
+}
+
+function createEventsDataFromFineMappings(fineMappings, minFineCv, maxFineCv)
+{
+	let mappings = [];
+	
+	let numMappings = fineMappings.length;
+	for (let i = 0; i < numMappings; ++i)
+	{
+		let value = fineMappings[i]['conversion_value'];
+		if (value < minFineCv || value > maxFineCv) continue;
+		
+		mappings.push.apply(mappings, fineMappings[i]['events']);
+	}
+	
+	if (mappings.length === 0) return null;
+	
+	let mappingGroups = groupBy(mappings, 'event_name');
+
+	let ret = [];
+	
+	for (let eventName in mappingGroups)
+	{
+		let eventsData = groupEventsData(eventName, mappingGroups[eventName]);
+		if (eventsData && eventsData.length > 0)
+		{
+			ret.push.apply(ret, eventsData);
+		}
+	}
+	
+	if (ret.length === 0) return null;
+	
+	return ret;
+}
+
+function createEventData(baseObject, fineMappings)
 {
 	let mappingType = baseObject['mapping_type'];
 	if (!mappingType) return null;
@@ -93,9 +434,25 @@ function createEventData(baseObject)
 		countMax = baseObject['max_session_count'];
 		if (countMax === undefined) countMax = null;
 	}
-	else if (mappingType === 'session')
+	else if (mappingType === 'fine_cv')
 	{
-		//TODO
+		if (!fineMappings) return null;
+		
+		let minFineCv = baseObject['min_fine_cv'];
+		if (minFineCv === undefined) minFineCv = 0;
+		
+		if (minFineCv < 0) return null;
+		else if (minFineCv > 63) return null;
+		
+		let maxFineCv = baseObject['max_fine_cv'];
+		if (maxFineCv === undefined) maxFineCv = 63;
+
+		if (maxFineCv < 0) return null;
+		else if (maxFineCv > 63) return null;
+		
+		if (maxFineCv < minFineCv) return null;
+		
+		return createEventsDataFromFineMappings(fineMappings, minFineCv, maxFineCv);
 	}
 	else
 	{
@@ -105,16 +462,16 @@ function createEventData(baseObject)
 	let ret = {};
 	
 	ret['event_name'] = eventName;
-	
+
 	if (revenueMin !== null || revenueMax !== null)
 	{
-		ret['revenue_min'] = revenueMin !== null ? revenueMin : 0;
+		if (revenueMin !== null) ret['revenue_min'] = Math.max(revenueMin, 0.0);
 		if (revenueMax !== null) ret['revenue_max'] = revenueMax;
 	}
 	
 	if (countMin !== null || countMax !== null)
 	{
-		ret['count_min'] = countMin !== null ? countMin : 0;
+		if (countMin !== null) ret['count_min'] = Math.max(countMin, 1);
 		if (countMax !== null) ret['count_max'] = countMax;
 	}
 	
@@ -126,8 +483,8 @@ function createFineMappingData(baseObject)
 	let ret = {};
 	
 	let value = baseObject['fine_cv'];
-	if (!value) return null;
-
+	if (value === undefined || value === null) return null;
+	
 	let mappings = baseObject['value_mappings'];
 	if (!mappings) return null;
 
@@ -136,10 +493,17 @@ function createFineMappingData(baseObject)
 	let numMappings = mappings.length;
 	for (let i = 0; i < numMappings; ++i)
 	{
-		let eventData = createEventData(mappings[i]);
+		let eventData = createEventData(mappings[i], null);
 		if (eventData)
 		{
-			eventsData.push(eventData);
+			if (eventData instanceof Array)
+			{
+				eventsData.push.apply(eventsData, eventData);
+			}
+			else
+			{
+				eventsData.push(eventData);
+			}
 		}
 	}
 	
@@ -166,7 +530,7 @@ function createFineMappingsData(baseObject)
 	return ret;
 }
 
-function createCoarseMappingData(baseObject)
+function createCoarseMappingData(baseObject, fineMappings)
 {
 	let ret = {};
 	
@@ -181,10 +545,17 @@ function createCoarseMappingData(baseObject)
 	let numMappings = mappings.length;
 	for (let i = 0; i < numMappings; ++i)
 	{
-		let eventData = createEventData(mappings[i]);
+		let eventData = createEventData(mappings[i], fineMappings);
 		if (eventData)
 		{
-			eventsData.push(eventData);
+			if (eventData instanceof Array)
+			{
+				eventsData.push.apply(eventsData, eventData);
+			}
+			else
+			{
+				eventsData.push(eventData);
+			}
 		}
 	}
 	
@@ -196,14 +567,14 @@ function createCoarseMappingData(baseObject)
 	return ret;
 }
 
-function createCoarseMappingsData(baseObject)
+function createCoarseMappingsData(baseObject, fineMappings)
 {
 	let ret = [];
 	
 	let num = baseObject.length;
 	for (let i = 0; i < num; ++i)
 	{
-		let data = createCoarseMappingData(baseObject[i]);
+		let data = createCoarseMappingData(baseObject[i], fineMappings);
 		if (data) ret.push(data);
 	}
 	
@@ -211,15 +582,69 @@ function createCoarseMappingsData(baseObject)
 	return ret;
 }
 
-function createSettingsData(baseObject)
+function getTimeLockFromCondition(lockCondition, minLockHours, maxLockHours)
 {
-	let ret = {};
-	//TODO
+	if (lockCondition['lock_condition_type'] !== 'time') return null;
+	
+	let seconds = lockCondition['post_install_time'];
+	
+	let hours = Math.floor(seconds / 3600); //convert to hours
+	
+	if (hours >= maxLockHours) return null;
+	
+	if (hours < minLockHours) hours = minLockHours;
+	
+	return hours - minLockHours + 1;
+}
+
+function getTimeLockFromConditions(lockConditions, minLockHours, maxLockHours)
+{
+	let ret = null;
+	
+	let num = lockConditions.length;
+	for (let i = 0; i < num; ++i)
+	{
+		let timeLock = getTimeLockFromCondition(lockConditions[i], minLockHours, maxLockHours);
+		if (timeLock)
+		{
+			if (!ret) ret = timeLock;
+			else ret = Math.min(timeLock, ret);
+		}
+	}
 	
 	return ret;
 }
 
-function createWindowData(baseObject, id, hasFine, hasCoarse)
+function createSettingsData(baseObject, minLockHours, maxLockHours)
+{
+	let ret = {};
+	ret['app_store_id'] = appStoreId;
+	ret['updated_at'] = updatedTime;
+	ret['reporting_currency'] = baseCurrency;
+	
+	let lockConditions = baseObject['lock_conditions'];
+		
+	if (lockConditions)
+	{
+		let lockWindowObject = null;
+		
+		let timeLock = getTimeLockFromConditions(lockConditions, minLockHours, maxLockHours);
+		if (timeLock)
+		{
+			if (lockWindowObject === null) lockWindowObject = {};
+			lockWindowObject['time_in_hours'] = timeLock;
+		}
+		
+		if (lockWindowObject)
+		{
+			ret['lock_window'] = lockWindowObject;
+		}
+	}
+	
+	return ret;
+}
+
+function createWindowData(baseObject, id, hasFine, hasCoarse, fineMappings, minLockHours, maxLockHours)
 {
 	let ret = {}
 	
@@ -232,6 +657,11 @@ function createWindowData(baseObject, id, hasFine, hasCoarse)
 		{
 			let fineData = createFineMappingsData(mappings);
 			data['fine'] = fineData || {};
+						
+			if (fineMappings == null && fineData)
+			{
+				fineMappings = fineData;
+			}
 		}
 	}	
 	
@@ -241,12 +671,12 @@ function createWindowData(baseObject, id, hasFine, hasCoarse)
 		
 		if (mappings)
 		{
-			let coarseData = createCoarseMappingsData(mappings);
+			let coarseData = createCoarseMappingsData(mappings, fineMappings);
 			data['coarse'] = coarseData || {};
 		}
 	}
 
-	let settingsData = createSettingsData(baseObject);
+	let settingsData = createSettingsData(baseObject, minLockHours, maxLockHours);
 	data['settings'] = settingsData;
 		
 	ret['data'] = data;
@@ -259,22 +689,37 @@ router.get('/', async function(req, res, next) {
   try {
 	let ret = [];
 	
+	let fineMappings = null;
+	
 	let windowOne = skanSchema['window_one'];
 	if (windowOne)
 	{
-		ret.push(createWindowData(windowOne, 1, true, true));
+		//console.log("WINDOW ONE");
+		
+		let data = createWindowData(windowOne, 1, true, true, null, 1, 48);
+		
+		fineMappings = data['data']['fine'];
+		if (fineMappings === undefined) fineMappings = null;
+		
+		ret.push(data);
 	}
 	
 	let windowTwo = skanSchema['window_two'];
 	if (windowTwo)
 	{
-		ret.push(createWindowData(windowTwo, 2, false, true));
+		//console.log("WINDOW TWO");
+		
+		let data = createWindowData(windowTwo, 2, false, true, fineMappings, 49, 168);
+		ret.push(data);
 	}
 		
 	let windowThree = skanSchema['window_three'];
 	if ('window_three')
 	{
-		ret.push(createWindowData(windowThree, 3, false, true));
+		//console.log("WINDOW THREE");
+		
+		let data = createWindowData(windowThree, 3, false, true, fineMappings, 169, 840);
+		ret.push(data);
 	}
 	
     res.json(ret);
