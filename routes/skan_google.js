76be5dd5d71df82aa771209c3614ab2e5202e4d2
Nguyễn Chí Thiện <ncthien@superfine.org>
2024-03-26 21:59:53 +0700

    Test getting SKAN schema from with Google Analytics Admin API

--- routes/skan_google.js
+++ routes/skan_google.js
@@ -2,15 +2,20 @@ import express from 'express';
 import fetch from 'node-fetch';
 import * as fs from 'fs';
 
+import { parseTemplate } from 'url-template';
+
 const router = express.Router();
 
 const GOOGLE_TOKEN_URL = 'https://www.googleapis.com/oauth2/v4/token';
 
 const clientId = '420137273716-pbc6p4542ipbd4hi72sj6tokk74asgcf.apps.googleusercontent.com';
 const clientSecret = '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';
-const refreshToken = '1//044D-XBUuseHNCgYIARAAGAQSNwF-L9Ir0EFEmL7z2XAGwrwMJpkwlgJh8aBor5Ad7naVUoaaBOIA9qRQIExJilIltm9roNzCJFs';
+const refreshToken = '1//04378p8o-bALsCgYIARAAGAQSNwF-L9IrVVgsp7qyUzpmFHEkh7Y1lORHie1wKBM8qsRzewwJ-Y4IZpJ_7ChhPs7lz5l4DEP7f6U';
 
-const developerToken = 'A-8CfsVKc6zkNJhLLcFPsA';
+const SKADNETWORK_CONVERSION_VALUE_SCHEMA_GET = parseTemplate('https://analyticsadmin.googleapis.com/v1alpha/properties/{property_id}/dataStreams/{dataStream}/sKAdNetworkConversionValueSchema');
+
+const propertyId = '415249261';
+const dataStream = '7779582138';
 
 const skanSchema = JSON.parse(fs.readFileSync('./config/skan_v4_test.json'));
 
@@ -31,17 +36,21 @@ async function setupSkan(accessToken)
 {
 	const authorization = 'Bearer ' + accessToken;
 	
-	const response = await fetch('https://googleads.googleapis.com/v16/customers:listAccessibleCustomers',
+	const url = SKADNETWORK_CONVERSION_VALUE_SCHEMA_GET.expand({
+		property_id: propertyId,
+		dataStream: dataStream
+	});
+	
+	const response = await fetch(url,
 	{
 		method: 'GET',
 		headers: {
-		  'Authorization': authorization,
-		  'developer-token': developerToken
+		  'Authorization': authorization
 		}
 	});  
 	
 	const responseJSON = await response.json();
-	console.log(responseJSON);
+	console.log(JSON.stringify(responseJSON));
 }
 
 router.get('/', async function(req, res, next) {
